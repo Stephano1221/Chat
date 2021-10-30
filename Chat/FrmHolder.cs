47d5a454cc37fe45c6ac19702e545e364b8db6dd
Nathan Cole <46795318+Stephano1221@users.noreply.github.com>
2021-10-30 18:17:08 +0100

    Allow specifying whether pre-release clients/servers are supported Specifies whether connecting clients/servers are allowed to be pre-release version numbers or not.

--- Chat/FrmHolder.cs
+++ Chat/FrmHolder.cs
@@ -20,8 +20,10 @@ namespace Chat
         public static string applicationVersion = "0.1.0-alpha"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. To use versions from the assembly (i.e. AssemblyInfo.cs): Application.ProductVersion;
         public static string minimumSupportedClientVersion = "0.1.0"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. As a server, clients connecting must be of version equal to or greater than this.
         public static string maximumSupportedClientVersion = minimumSupportedClientVersion;
+        public static bool allowClientPreRelease = true;
         public static string minumumSupportedServerVersion = "0.1.0"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. As a client, servers connecting must be of version equal to or greater than this.
         public static string maximumSupportedServerVersion = minumumSupportedServerVersion;
+        public static bool allowServerPreRelease = true;
 
         private FrmLoginScreen loginScreen;
 
