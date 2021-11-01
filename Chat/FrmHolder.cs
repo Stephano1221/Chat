4a60dcc55711bddd5c13fbc5958ea89ff335d176
Nathan Cole <46795318+Stephano1221@users.noreply.github.com>
2021-11-01 20:26:25 +0000

    Set the minimum supported client version to 0.1.0-alpha The current version targets 0.1.0-alpha.
    Set the minimum supported client version to 0.1.0-alpha.

--- Chat/FrmHolder.cs
+++ Chat/FrmHolder.cs
@@ -18,7 +18,7 @@ namespace Chat
         public static int clientId = -1;
         public static string applicationWindowText = "Chat";
         public static string applicationVersion = "0.1.0-alpha"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. To use versions from the assembly (i.e. AssemblyInfo.cs): Application.ProductVersion;
-        public static string minimumSupportedClientVersion = "0.1.0"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. As a server, clients connecting must be of version equal to or greater than this.
+        public static string minimumSupportedClientVersion = "0.1.0-alpha"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. As a server, clients connecting must be of version equal to or greater than this.
         public static string maximumSupportedClientVersion = minimumSupportedClientVersion;
         public static bool allowClientPreRelease = true;
         public static string minumumSupportedServerVersion = "0.1.0"; //Format: [Major].[Minor].[Patch](optional:-[Pre-Release]). Follows semantic versioning 2.0.0. As a client, servers connecting must be of version equal to or greater than this.
