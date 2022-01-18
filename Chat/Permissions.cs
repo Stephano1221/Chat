205758d6d84c8719c2b0979a2757826cea88a368
Nathan Cole <46795318+Stephano1221@users.noreply.github.com>
2022-01-18 21:07:35 +0000

    Add permissions numbers Permissions numbers would allow a rank to determine what permissions that it has from a single number, provided that adding and subtracting each individual permission number produces a unique outcome that no other such combination can provide.
    This allows the permissions of each rank to be stored without needing separate database tables for each permission.
    
    Add permissions numbers.

---
+++ Chat/Permissions.cs
@@ -0,0 +1,33 @@
+﻿namespace Chat
+{
+    public static class Permissions
+    {
+        public enum IndividualPermissionNumber : ulong
+        {
+            None = 0,
+            Overseer = 1,
+            Kick = 2,
+            Ban = 4,
+            Mute = 8,
+            Deafen = 16,
+            DeleteMessages = 32,
+            RegulateChannels = 64,
+            RegulateRanks = 128,
+            PingAll = 256,
+            ReadMessages = 512,
+            SendMessages = 1024,
+            HearVoice = 2048,
+            SendVoice = 4096
+        }
+
+        public static ulong GetPermissionsNumber(IndividualPermissionNumber[] individualPermissionNumbers)
+        {
+            ulong permissionNumber = 0;
+            foreach (IndividualPermissionNumber individualPermissionNumber in individualPermissionNumbers)
+            {
+                permissionNumber += Convert.ToUInt64(individualPermissionNumber);
+            }
+            return permissionNumber;
+        }
+    }
+}
