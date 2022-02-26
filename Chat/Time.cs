6a7347c610519fdf18272e1aee092a2e51cce4af
Nathan Cole <46795318+Stephano1221@users.noreply.github.com>
2022-02-26 14:04:39 +0000

    Add Time class Add a class which allows us to obtain the current UTC time, expressed in Ticks, Milliseconds or Ticks/Milliseconds since epoch.

---
+++ Chat/Time.cs
@@ -0,0 +1,29 @@
+﻿namespace Chat
+{
+    public static class Time
+    {
+        const ulong EpochMillisecond = 63776592000000; //01/01/2022 12:00AM UTC
+        const ulong TicksPerMillisecond = TimeSpan.TicksPerMillisecond;
+        const ulong EpochTick = EpochMillisecond * TicksPerMillisecond;
+
+        public static ulong UtcTicksNow()
+        {
+            return Convert.ToUInt64(DateTime.UtcNow.Ticks);
+        }
+
+        public static ulong UtcTicksSinceEpoch()
+        {
+            return UtcTicksNow() - EpochTick;
+        }
+
+        public static ulong UtcMillisecondsNow()
+        {
+            return UtcTicksNow() / TicksPerMillisecond;
+        }
+
+        public static ulong UtcMillisecondsSinceEpoch()
+        {
+            return UtcMillisecondsNow() - EpochMillisecond;
+        }
+    }
+}
