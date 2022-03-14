f99cc9fa76a434c4d30ff541790dbebe2aa88366
Nathan Cole <46795318+Stephano1221@users.noreply.github.com>
2022-03-14 20:58:51 +0000

    Add custom JSON converter for BigInteger IDs, as BigInteger types, cannot be natively serialized/converted using JsonConverter.
    Add a custom JsonConverter derived class which can convert to/from BigIntegers and strings.

---
+++ Chat/CustomJsonConverters.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Chat
+{
+    public class BigIntegerJsonConverter : JsonConverter<BigInteger>
+    {
+        public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return BigInteger.Parse(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
